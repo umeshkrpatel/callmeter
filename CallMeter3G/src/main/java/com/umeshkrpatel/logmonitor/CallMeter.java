7dc4de64607dc71de738df7022af151675b207de
Umesh Kumar Patel <umeshkrpatel@gmail.com>
2016-01-05 09:33:44 +0530

    Update Package Name

---
+++ CallMeter3G/src/main/java/com/umeshkrpatel/logmonitor/CallMeter.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright (C) 2009-2013 Felix Bechstein
+ * 
+ * This file is part of Call Meter 3G.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.umeshkrpatel.logmonitor;
+
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import de.ub0r.android.callmeter.R;
+import de.ub0r.android.lib.Utils;
+import de.ub0r.android.logg0r.Log;
+
+/**
+ * @author flx
+ */
+public final class CallMeter extends Application {
+
+    /**
+     * Minimum date.
+     */
+    public static final long MIN_DATE = 10000000000L;
+    /**
+     * Milliseconds per seconds.
+     */
+    public static final long MILLIS = 1000L;
+    /**
+     * 80.
+     */
+    public static final int EIGHTY = 80;
+    /**
+     * 100.
+     */
+    public static final int HUNDRED = 100;
+    /**
+     * Seconds of a minute.
+     */
+    public static final int SECONDS_MINUTE = 60;
+    /**
+     * Seconds of a hour.
+     */
+    public static final int SECONDS_HOUR = 60 * SECONDS_MINUTE;
+    /**
+     * Seconds of a day.
+     */
+    public static final int SECONDS_DAY = 24 * SECONDS_HOUR;
+    /**
+     * 10.
+     */
+    public static final int TEN = 10;
+    /**
+     * Bytes: kB.
+     */
+    public static final long BYTE_KB = 1024L;
+    /**
+     * Bytes: MB.
+     */
+    public static final long BYTE_MB = BYTE_KB * BYTE_KB;
+    /**
+     * Bytes: GB.
+     */
+    public static final long BYTE_GB = BYTE_MB * BYTE_KB;
+    /**
+     * Bytes: TB.
+     */
+    public static final long BYTE_TB = BYTE_GB * BYTE_KB;
+    private static final String TAG = "CallMeter";
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public static void setActivitySubtitle(final Activity a, final String t) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            a.getActionBar().setSubtitle(t);
+        }
+    }
+
+    public static boolean hasPermission(final Context context, final String permission) {
+        return
+                Build.VERSION.SDK_INT < Build.VERSION_CODES.M
+                        || ContextCompat.checkSelfPermission(context, permission)
+                        == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static boolean hasPermissions(final Context context, final String... permissions) {
+        for (String p : permissions) {
+            if (!hasPermission(context, p)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean requestPermission(final Activity activity, final String permission,
+                                            final int requestCode, final int message,
+                                            final DialogInterface.OnClickListener onCancelListener) {
+        Log.i(TAG, "requesting permission: " + permission);
+        if (!hasPermission(activity, permission)) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                new AlertDialog.Builder(activity)
+                        .setTitle(R.string.permissions_)
+                        .setMessage(message)
+                        .setCancelable(false)
+                        .setNegativeButton(android.R.string.cancel, onCancelListener)
+                        .setPositiveButton(android.R.string.ok,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(final DialogInterface dialogInterface,
+                                                        final int i) {
+                                        ActivityCompat.requestPermissions(activity,
+                                                new String[]{permission}, requestCode);
+                                    }
+                                })
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(activity, new String[]{permission}, requestCode);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1) {
+            // fix AsyncTask for some old devices + broken gms
+            // http://stackoverflow.com/a/27239869/2331953
+            try {
+                Class.forName("android.os.AsyncTask");
+            } catch (Throwable ignore) {
+            }
+        }
+
+        super.onCreate();
+        Utils.setLocale(this);
+    }
+}
---
+++ CallMeter3G/src/main/java/com/umeshkrpatel/logmonitor/ui/prefs/CVDatePreference.java
@@ -0,0 +1,188 @@
+package com.umeshkrpatel.logmonitor.ui.prefs;
+
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.ContentValues;
+import android.content.Context;
+import android.os.Build;
+import android.preference.DialogPreference;
+import android.preference.PreferenceManager;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+import de.ub0r.android.callmeter.R;
+import de.ub0r.android.logg0r.Log;
+
+/**
+ * DatePreference holding it's value in {@link ContentValues}.
+ *
+ * @author flx
+ */
+public final class CVDatePreference extends DialogPreference implements OnTimeSetListener {
+
+    private static final String TAG = "CVDatePreference";
+
+    /**
+     * {@link ContentValues} for saving values.
+     */
+    private final ContentValues cv;
+
+    /**
+     * {@link UpdateListener}.
+     */
+    private final UpdateListener ul;
+
+    /**
+     * Show date and time?
+     */
+    private final boolean dt;
+
+    /**
+     * Current date.
+     */
+    private final Calendar v = Calendar.getInstance();
+    /**
+     * Show help.
+     */
+    private final boolean sh;
+    /**
+     * {@link DatePicker}.
+     */
+    private DatePicker dp = null;
+
+    /**
+     * Default constructor.
+     *
+     * @param context         {@link Context}
+     * @param values          {@link ContentValues}
+     * @param key             key
+     * @param showDateAndTime Show date and time?
+     */
+    public CVDatePreference(final Context context, final ContentValues values, final String key,
+                            final boolean showDateAndTime) {
+        super(context, null);
+        setPersistent(false);
+        setKey(key);
+        cv = values;
+        dt = showDateAndTime;
+        sh = PreferenceManager.getDefaultSharedPreferences(context).getBoolean(
+                Preferences.PREFS_SHOWHELP, true);
+        if (context instanceof UpdateListener) {
+            ul = (UpdateListener) context;
+        } else {
+            ul = null;
+        }
+    }
+
+    @Override
+    public void setTitle(final int titleResId) {
+        super.setTitle(titleResId);
+        setDialogTitle(titleResId);
+    }
+
+    @Override
+    public void setTitle(final CharSequence title) {
+        super.setTitle(title);
+        setDialogTitle(title);
+    }
+
+    @Override
+    protected View onCreateView(final ViewGroup parent) {
+        View view = super.onCreateView(parent);
+        TextView tv = (TextView) view.findViewById(android.R.id.summary);
+        if (tv != null) {
+            tv.setMaxLines(Integer.MAX_VALUE);
+        }
+        return view;
+    }
+
+    @Override
+    protected View onCreateDialogView() {
+        dp = new DatePicker(getContext());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            dp.setCalendarViewShown(true);
+            dp.setSpinnersShown(false);
+            dp.setMaxDate(System.currentTimeMillis());
+        }
+        updateDialog();
+        return dp;
+    }
+
+    /**
+     * Set date and time as unix time stamp.
+     *
+     * @param time time stamp
+     */
+    public void setValue(final long time) {
+        v.setTimeInMillis(time);
+        if (!this.sh) {
+            setSummary(getContext().getString(R.string.value) + ": "
+                    + DateFormat.getDateFormat(getContext()).format(v.getTime()));
+        }
+        updateDialog();
+    }
+
+    /**
+     * Set date and time as unix time stamp.
+     *
+     * @param time calendar
+     */
+    public void setValue(final Calendar time) {
+        if (time == null) {
+            setValue(System.currentTimeMillis());
+        } else {
+            setValue(time.getTimeInMillis());
+        }
+    }
+
+    /**
+     * Update {@link DatePicker}.
+     */
+    private void updateDialog() {
+        if (dp == null) {
+            return;
+        }
+        try {
+            dp.updateDate(v.get(Calendar.YEAR), v.get(Calendar.MONTH),
+                    v.get(Calendar.DAY_OF_MONTH));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            Log.e(TAG, "date array out of bound", e);
+            v.set(Calendar.YEAR, Calendar.getInstance().get(Calendar.YEAR));
+            setValue(v);
+        }
+    }
+
+    @Override
+    protected void onDialogClosed(final boolean positiveResult) {
+        if (positiveResult) {
+            v.set(dp.getYear(), dp.getMonth(), dp.getDayOfMonth());
+            cv.put(getKey(), v.getTimeInMillis());
+            if (ul != null) {
+                ul.onUpdateValue(this);
+            }
+            if (dt) {
+                TimePickerDialog tpd = new TimePickerDialog(getContext(), this,
+                        v.get(Calendar.HOUR_OF_DAY), v.get(Calendar.MINUTE), true);
+                tpd.setTitle(getTitle());
+                tpd.setCancelable(true);
+                tpd.show();
+            }
+        }
+    }
+
+    @Override
+    public void onTimeSet(final TimePicker view, final int hourOfDay, final int minute) {
+        v.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        v.set(Calendar.MINUTE, minute);
+        cv.put(getKey(), v.getTimeInMillis());
+        if (ul != null) {
+            ul.onUpdateValue(this);
+        }
+    }
+}
